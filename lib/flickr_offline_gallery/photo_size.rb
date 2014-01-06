c65745eb9dcaef2e4771ada45aacd4f47db06c31
Lucas Parry <lparry@gmail.com>
2014-01-06 17:27:20 +0000

    Stop being an OpenStruct
    That was begging for trouble

--- lib/flickr_offline_gallery/photo_size.rb
+++ lib/flickr_offline_gallery/photo_size.rb
@@ -1,20 +1,13 @@
 module FlickrOfflineGallery
-  class PhotoSize < OpenStruct
-    # attrs: label, width, height, source
+  class PhotoSize
+
+    attr_reader :label, :width, :height, :source
+
     def initialize(horrible_flickraw_response_junk)
-      super(horrible_flickraw_response_junk.to_hash)
-    end
-
-    def height
-      super.to_i
-    end
-
-    def width
-      super.to_i
-    end
-
-    def url
-      source
+      @label  = horrible_flickraw_response_junk.label
+      @height = horrible_flickraw_response_junk.height
+      @width  = horrible_flickraw_response_junk.width
+      @source = horrible_flickraw_response_junk.source
     end
 
     def key
