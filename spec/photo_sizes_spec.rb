d31d97ca3ce479f7978b32fd16906013cb529860
Lucas Parry <lparry@gmail.com>
2014-01-06 19:26:10 +0000

    Spec existing (horrible) behaviour

--- spec/photo_sizes_spec.rb
+++ spec/photo_sizes_spec.rb
@@ -1,10 +1,28 @@
 require 'spec_helper'
 
-describe FlickrOfflineGallery::PhotoSize do
-  it 'should record a vcr tape' do
-    VCR.use_cassette('photoset') do
-      photoset = FlickrOfflineGallery::Photoset.new("72157639475533743")
-      FlickrOfflineGallery.render_photoset(photoset, "thumbnail")
+module FlickrOfflineGallery
+  describe PhotoSizes do
+
+    let(:raw_flickr_response) do
+      VCR.use_cassette('photo_sizes') do
+        FlickrAPI.get_photo_sizes("10440808526")
+      end
+    end
+
+    subject!(:sizes) { described_class.new(raw_flickr_response) }
+
+    it "should allow access like an array" do
+      sizes["thumbnail"].should be_a(PhotoSize)
+      sizes["thumbnail"].label.should == "Thumbnail"
+    end
+
+    it "should respond to #each" do
+      sizes.should respond_to(:each)
+    end
+
+    it "should be an openstuct (I hate this already, it'll be the first to go once the specs are done)" do
+      sizes.thumbnail.should be_a(PhotoSize)
+      sizes["thumbnail"].label.should == "Thumbnail"
     end
   end
 end
